78f940cb9ce49f5bd525422a3c3cba8b6216d820
Victor Fedorov <vpfedorov@bk.ru>
2022-05-16 00:21:56 +0300

    Heroes list
    * add heroes list (*ngFor)
    * add ngIf

---
+++ herous-angular/src/app/heroes/heroes.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
+
+@Component({
+  // метаданные
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+
+export class HeroesComponent implements OnInit {
+
+  constructor() { }
+
+  // то же что ComponentDidMount ...
+  // lifecycle method
+  ngOnInit() {
+  }
+
+  heroes = HEROES;
+
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+}
+
