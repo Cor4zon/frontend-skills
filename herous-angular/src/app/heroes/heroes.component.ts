60058167711e1e9830f9ca991b81f7ddc1eef4dc
Victor Fedorov <vpfedorov@bk.ru>
2022-05-16 10:33:21 +0300

    async version of heroService

--- herous-angular/src/app/heroes/heroes.component.ts
+++ herous-angular/src/app/heroes/heroes.component.ts
@@ -3,28 +3,37 @@ import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
 import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
 
+import { HeroService } from '../hero.service';
+
 @Component({
   // метаданные
   selector: 'app-heroes',
+  providers: [HeroService],
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
 
 export class HeroesComponent implements OnInit {
 
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
   // то же что ComponentDidMount ...
   // lifecycle method
   ngOnInit() {
+    this.getHeroes();
   }
 
-  heroes = HEROES;
+  heroes: Hero[] = [];
+
 
   selectedHero?: Hero;
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
 
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+  }
 }
 
